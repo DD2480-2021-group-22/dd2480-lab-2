179b24ababfe5b4078c50fe156bab3ae96c5a05a
kaniyi <kaniyi@kth.se>
2021-02-11 23:40:51 +0100

    refractor #16: Change so that build webpage is done as a redirect. Change so that DocumentBuilder returns HTML string

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,6 +1,11 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import jakarta.servlet.ServletException;
@@ -9,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jgit.revwalk.DepthWalk;
 
 /**
  * A continuous integration server which listens to Github push events.
@@ -35,33 +41,49 @@ public class ContinuousIntegrationServer extends AbstractHandler
             HttpServletRequest request,
             HttpServletResponse response)
             throws IOException, ServletException {
-
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(javax.servlet.http.HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
 
-        // see https://stackoverflow.com/questions/8100634/get-the-post-request-body-from-httpservletrequest
-        String reqString = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        if(target.equals("/builds")){
+            System.out.println("Inne");
+            DocumentBuilder db = new DocumentBuilder();
+            try {
+                String html = db.writeDoc();
+                response.getWriter().println(html);
+                response.flushBuffer();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
 
-        Payload payload = Payload.parse(reqString);
+        }
+        else{
+            baseRequest.setHandled(true);
 
-        File dir = Files.createTempDirectory("temp").toFile();
-        try {
-            // Clones repo
-            File repo = new RepoSnapshot(payload).cloneFiles(dir);
-            // Executes gradle build
-            Report buildReport = GradleHandler.build(repo);
-            // Sends mail
-            Mailserver mailserver = new Mailserver();
-            mailserver.useGmailSMTP();
-            SendMail sendMail = new SendMail();
-            sendMail.sendMail(buildReport, payload, mailserver, payload.getPusherEmail(), "Hello");
+            // see https://stackoverflow.com/questions/8100634/get-the-post-request-body-from-httpservletrequest
+            String reqString = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 
-        } catch (Exception e) {
-            System.out.println("Failed to process repo: " + e.getMessage());
+            Payload payload = Payload.parse(reqString);
+
+            File dir = Files.createTempDirectory("temp").toFile();
+            try {
+                // Clones repo
+                File repo = new RepoSnapshot(payload).cloneFiles(dir);
+                // Executes gradle build
+                Report buildReport = GradleHandler.build(repo);
+                // Sends mail
+                Mailserver mailserver = new Mailserver();
+                mailserver.useGmailSMTP();
+                SendMail sendMail = new SendMail();
+                sendMail.sendMail(buildReport, payload, mailserver, payload.getPusherEmail(), "Hello");
+
+            } catch (Exception e) {
+                System.out.println("Failed to process repo: " + e.getMessage());
+            }
+
+            response.getWriter().println("CI job done");
         }
 
-        response.getWriter().println("CI job done");
+
     }
 
     // used to start the CI server in command line
@@ -73,8 +95,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
      */
     public static void main(String[] args) throws Exception
     {
-        DocumentBuilder db = new DocumentBuilder();
-        db.writeDoc("1", "2", "3", "4", "5");
+
         Server server = new Server(8080);
         server.setHandler(new ContinuousIntegrationServer());
         server.start();
--- src/main/java/DocumentBuilder.java
+++ src/main/java/DocumentBuilder.java
@@ -18,96 +18,89 @@ public class DocumentBuilder {
     /**
      * Generates an HTML report by using bufferedwriter to write string objects,
      * formatted to look like HTML syntax, to a file.
-     *
-     * @param commitID
-     * @param buildDate
-     * @param testResult
-     * @param buildStatus
-     * @param buildLogs
      */
-    public void writeDoc(String commitID, String buildDate, String testResult, String buildStatus, String buildLogs) {
+    public String writeDoc() throws SQLException {
+        MysqlDatabase myDb = new MysqlDatabase();
+        List<CommitStructure> mockCommits = myDb.selectAllCommits();
         String start = "<!DOCTYPE html>\n<html><head>\n" +
                 "<link rel=\"stylesheet\" href=\"styles.css\">\n" +
                 "</head>\n<body>\n";
         String heading = "<h1 align=\"center\">Build Report</h1>\n";
+        String css = "<style>" +
+                "h1 {\n" +
+                "    padding-top: 40px;\n" +
+                "    padding-bottom: 30px;\n" +
+                "}\n" +
+                "\n" +
+                "body {\n" +
+                "  font-family: Arial, sans-serif;\n" +
+                "  background-color: #a7e7fc;\n" +
+                "}\n" +
+                "\n" +
+                ".field {\n" +
+                "    display: inline-block;\n" +
+                "    padding:0px 10px;\n" +
+                "}\n" +
+                "\n" +
+                ".BuildContainer {\n" +
+                "    border: solid;\n" +
+                "    margin-bottom: 25px\n" +
+                "}\n" +
+                "\n" +
+                "#currentDiv {\n" +
+                "    margin-bottom: -15px;\n" +
+                "    margin-left: 100px;\n" +
+                "    margin-right: 100px;\n" +
+                "}\n" +
+                "\n" +
+                "#previousDiv {\n" +
+                "\n" +
+                "    margin-top: 50px;\n" +
+                "\n" +
+                "}\n" +
+                "\n" +
+                "</style>";
         String end = "</body>\n</html>";
 
-        File f = new File("/Users/keivanmatinzadeh/DD2480/dd2480-lab-2/src/Resources/index.html");
-
-        /*String currentBuild = String.format(
-                "<div id=\"currentDiv\" align=\"center\"><h2>Current Build</h2>" +
-                        "<div align=\"center\">" +
-                        "<div class=\"field\"><p><b>Commit id:</b> %s</p></div>" +
-                        "<div class=\"field\"><p><b>Build date:</b> %s</p></div>" +
-                        "<div class=\"field\"><p><b>Test result:</b> %s</p></div>" +
-                        "<div class=\"field\"><p><b>Build status:</b> %s</p></div>" +
-                        "<div class=\"field\"><p><b>Build logs:</b> %s</p></div>" +
-                        "</div>" + "</div>"
-            , "535532532555", "09/02-21", "Successful", "Successful", "fsfsfdf");
-
-        String previousBuilds = String.format("<div id=\"previousDiv\" align=\"center\"><h3>Previous Builds</h3>" +
-                "<form action='VERIFY.php' method='post'>" +
-                "<select name=\"PreviousBuilds\" id=\"dropdown\" onchange=\"handleSelect(this.form)\">" +
-                "<option value=\"previous build 1\">32525454353242325</option>" +
-                "<option value=\"previous build 2\">32525454353242325</option>"+
-                "</select></form><p><span id=\"output\"></span></p>" + "</div>\n"); */
-
-        List<CommitStructure> mockCommits = new ArrayList<>();
-
+        //So that we atleast have something to show if database is empty
         mockCommits.add(new CommitStructure("23424324432424", "11/02-20",
                 "logloglogsfsssfsfsfsfsffs", true));
-        mockCommits.add(new CommitStructure("23787824432424", "11/02-20",
-                "logloglogsfsssfsfsfsfsffs", true));
-        mockCommits.add(new CommitStructure("94859348598943", "11/02-20",
-                "logloglogsfsssfsfsfsfsffs", true));
-        mockCommits.add(new CommitStructure("00909895859858", "11/02-20",
-                "logloglogsfsssfsfsfsfsffs", true));
-        mockCommits.add(new CommitStructure("12312433000900", "11/02-20",
-                "logloglogsfsssfsfsfsfsffs", true));
 
-        String script = String.format("<script type=\"text/javascript\">\n" +
+        String script = "<script type=\"text/javascript\">\n" +
                 "function handleSelect(myForm)\n" +
                 "{ \n" +
                 "    var classCode = myForm.PreviousBuilds.value;\n" +
                 "    document.getElementById('output').innerText = classCode;\n" +
                 "}\n" +
-                "</script>");
+                "</script>";
 
-        String build = "<div id=\"currentDiv\" align=\"center\"><h2> Commits </h2>";
+        StringBuilder build = new StringBuilder("<div id=\"currentDiv\" align=\"center\"><h2> Commits </h2>");
+
+        for (CommitStructure mockCommit : mockCommits) {
+            build.append(String.format(
+                    "<div class=\"BuildContainer\" align=\"center\" id='%s'>" +
+                            "<div class=\"field\"><p><b>Commit id:</b> %s</p></div>" +
+                            "<div class=\"field\"><p><b>Build date:</b> %s</p></div>" +
+                            "<div class=\"field\"><p><b>Build status:</b> %s</p></div>" +
+                            "</div>"
+                    , mockCommit.getCommitID(), mockCommit.getCommitID(), mockCommit.getBuildDate(),
+                    mockCommit.isBuildResult()));
+        }
+        build.append("</div>");
+        /*
         try {
-            MysqlDatabase db = new MysqlDatabase();
-            List<CommitStructure> commits = new ArrayList<CommitStructure>();
-            commits = db.selectAllCommits();
-
-            for (int i = 0; i < mockCommits.size(); i++) {
-                 build += String.format(
-                                "<div class=\"BuildContainer\" align=\"center\">" +
-                                "<div class=\"field\"><p><b>Commit id:</b> %s</p></div>" +
-                                "<div class=\"field\"><p><b>Build date:</b> %s</p></div>" +
-                                "<div class=\"field\"><p><b>Build status:</b> %s</p></div>" +
-                                "<div class=\"field\"><p><b>Build logs:</b> %s</p></div>" +
-                                "</div>"
-                        , mockCommits.get(i).getCommitID(), mockCommits.get(i).getBuildDate(),
-                            mockCommits.get(i).isBuildResult(), mockCommits.get(i).getBuildLogs());
-            }
-            build += "</div>";
-        } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-
-        //"<div style=\"display: inline-block\"> <img src=\"img2.png\"> </div> </div>"
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("src/Resources/builds.html"));
             bw.write(start);
             bw.write(heading);
-            bw.write(build);
+            bw.write(String.valueOf(build));
             bw.write(script);
             bw.write(end);
             bw.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+
+        return start+heading+css+build+script+end;
     }
 
     //commit id	build date	test result (pass/fail)	build (pass/fail)	build logs
