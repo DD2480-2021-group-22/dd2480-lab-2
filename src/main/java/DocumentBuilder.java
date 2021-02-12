5b66ea74405cd84098c29e0f6c7856f25b8ba830
Keivan Matinzadeh <matinzadeh.keivan@gmail.com>
2021-02-12 16:16:29 +0100

    feat #16: Update css and html, create method for the log details page. Refactor the structure of the file; minimise code duplications by use of static variables.

--- src/main/java/DocumentBuilder.java
+++ src/main/java/DocumentBuilder.java
@@ -12,6 +12,54 @@ import java.util.List;
 
 public class DocumentBuilder {
 
+    private static String start = "<!DOCTYPE html>\n<html>\n<head>\n" +
+            "<title>Build Report</title>" +
+            "</head>\n<body>\n";
+    private static String css = "<style>" +
+            "h1 {\n" +
+            "    padding-top: 40px;\n" +
+            "    padding-bottom: 30px;\n" +
+            "}\n" +
+            "\n" +
+            "body {\n" +
+            "  font-family: Arial, sans-serif;\n" +
+            "  background-color: #a7e7fc;\n" +
+            "}\n" +
+            "\n" +
+            ".field {\n" +
+            "    display: inline-block;\n" +
+            "    padding:0px 10px;\n" +
+            "}\n" +
+            "\n" +
+            ".BuildContainer {\n" +
+            "    border: solid;\n" +
+            "    margin-bottom: 25px\n" +
+            "}\n" +
+            "\n" +
+            ".BuildDetailsContainer {\n" +
+            "    border-bottom: solid;\n" +
+            "    margin-bottom: 25px\n" +
+            "}\n" +
+            "\n" +
+            "#currentDiv {\n" +
+            "    margin-bottom: -15px;\n" +
+            "    margin-left: 100px;\n" +
+            "    margin-right: 100px;\n" +
+            "}\n" +
+            "\n" +
+            "#detailsMainDiv {\n" +
+            "    margin-left: 100px;\n" +
+            "    margin-right: 100px;\n" +
+            "}\n" +
+            "\n" +
+            "#previousDiv {\n" +
+            "\n" +
+            "    margin-top: 50px;\n" +
+            "\n" +
+            "}\n" +
+            "\n" +
+            "</style>";
+    private static String end = "</body>\n</html>";
     public DocumentBuilder() {
     }
 
@@ -19,83 +67,48 @@ public class DocumentBuilder {
      * Generates an HTML report by using bufferedwriter to write string objects,
      * formatted to look like HTML syntax, to a file.
      */
-    public String writeDoc() throws SQLException {
-        MysqlDatabase myDb = new MysqlDatabase();
-        List<CommitStructure> mockCommits = myDb.selectAllCommits();
-        String start = "<!DOCTYPE html>\n<html><head>\n" +
-                "<link rel=\"stylesheet\" href=\"styles.css\">\n" +
-                "</head>\n<body>\n";
+    public String writeDoc(List<CommitStructure> commits) throws SQLException {
         String heading = "<h1 align=\"center\">Build Report</h1>\n";
-        String css = "<style>" +
-                "h1 {\n" +
-                "    padding-top: 40px;\n" +
-                "    padding-bottom: 30px;\n" +
-                "}\n" +
-                "\n" +
-                "body {\n" +
-                "  font-family: Arial, sans-serif;\n" +
-                "  background-color: #a7e7fc;\n" +
-                "}\n" +
-                "\n" +
-                ".field {\n" +
-                "    display: inline-block;\n" +
-                "    padding:0px 10px;\n" +
-                "}\n" +
-                "\n" +
-                ".BuildContainer {\n" +
-                "    border: solid;\n" +
-                "    margin-bottom: 25px\n" +
-                "}\n" +
-                "\n" +
-                "#currentDiv {\n" +
-                "    margin-bottom: -15px;\n" +
-                "    margin-left: 100px;\n" +
-                "    margin-right: 100px;\n" +
-                "}\n" +
-                "\n" +
-                "#previousDiv {\n" +
-                "\n" +
-                "    margin-top: 50px;\n" +
-                "\n" +
-                "}\n" +
-                "\n" +
-                "</style>";
-        String end = "</body>\n</html>";
-
-        //So that we atleast have something to show if database is empty
-        mockCommits.add(new CommitStructure("23424324432424", "11/02-20",
-                "logloglogsfsssfsfsfsfsffs", true));
-
-        String script = "<script type=\"text/javascript\">\n" +
-                "function handleSelect(myForm)\n" +
-                "{ \n" +
-                "    var classCode = myForm.PreviousBuilds.value;\n" +
-                "    document.getElementById('output').innerText = classCode;\n" +
-                "}\n" +
-                "</script>";
 
         StringBuilder build = new StringBuilder("<div id=\"currentDiv\" align=\"center\"><h2> Commits </h2>");
 
-        for (CommitStructure mockCommit : mockCommits) {
+        for (CommitStructure commit : commits) {
+
             build.append(String.format(
-                    "<div class=\"BuildContainer\" align=\"center\" id='%s' onClick='redirect(this.id)'>" +
-                            "<div class=\"field\"><p><b>Commit id:</b> %s</p></div>" +
+                    "<div class=\"BuildContainer\" align=\"center\">" +
+                            "<div class=\"field\"><p><b>Commit id:</b> <a href=\"/commit?commitID=%s\">%s</a></p></div>" +
                             "<div class=\"field\"><p><b>Build date:</b> %s</p></div>" +
                             "<div class=\"field\"><p><b>Build status:</b> %s</p></div>" +
                             "</div>"
-                    , mockCommit.getCommitID(), mockCommit.getCommitID(), mockCommit.getBuildDate(),
-                    mockCommit.isBuildResult()));
+                    , commit.getCommitID(), commit.getCommitID(), commit.getBuildDate(),
+                    commit.isBuildResult()));
         }
         build.append("</div>");
 
-        String redirectScript = "<script type=\"text/javascript\">\n" +
-                "function redirect(commitID){\n" +
-                "window.location.href = 'http://localhost:8080/commit?commitID='+commitID;\n" +
-                "}" +
-                "</script>";
-
-        return start+heading+css+build+script+redirectScript+end;
+        return start+heading+css+build+end;
     }
 
-    //commit id	build date	test result (pass/fail)	build (pass/fail)	build logs
+    public String writeBuildDetails(CommitStructure commit){
+        String logs = commit.getBuildLogs();
+
+        String heading = "<h1 align=\"center\">Build Details</h1>\n";
+
+        String details = String.format(
+                "<div id=\"detailsMainDiv\" align=\"center\">" +
+                        "<div class=\"BuildDetailsContainer\" align=\"center\">" +
+                        "<div class=\"field\"><p><b>Commit id:</b><p>%s</p></div>" +
+                        "<div class=\"field\"><p><b>Build date:</b> %s</p></div>" +
+                        "<div class=\"field\"><p><b>Build status:</b> %s</p></div>" +
+                        "</div>" +
+                        "</div>"
+                , commit.getCommitID(), commit.getBuildDate(),
+                commit.isBuildResult());
+
+        String logData = "<div id=\"currentDiv\" align=\"center\"><h2> Log Data </h2>" +
+                "<div class=\"field\"><p>" + logs + "</p></div>";
+
+        return start+heading+css+details+logData+end;
+
+    }
+
 }
