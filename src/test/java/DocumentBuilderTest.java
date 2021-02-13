5e7d1cf3d0577f45f66873ec0a272752b944b336
Keivan Matinzadeh <matinzadeh.keivan@gmail.com>
2021-02-13 23:13:55 +0100

    test #16: Remove use of W3C HTML validator and add code for JTidy validator instead. Add one more test method to test the page for a specific build.

--- src/test/java/DocumentBuilderTest.java
+++ src/test/java/DocumentBuilderTest.java
@@ -1,17 +1,7 @@
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.safety.Whitelist;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
+import org.w3c.tidy.Tidy;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.sql.SQLException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +9,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DocumentBuilderTest {
 
+    DocumentBuilder db = new DocumentBuilder();
+
     /**
-     * This test checks that the commit hashes are inserted into the html document.
-     * HTML validation is performed by sending a request to https://validator.w3.org/nu/
-     * and the response contains a HTML document representing the web page if a HTML
-     * validation is performed at https://validator.w3.org/ manually. The HTML document
-     * returned should not contain the string "there were error", otherwise the validation fails.
+     * Test for the DocumentBuilder class.
+     * The first part of the test asserts that the returned HTML string from the method db.writeDoc contains
+     * the commit hash for the commits in the list.
+     *
+     * The second part of the test uses Jtidy to parse the string containing the HTML and check for errors.
+     * The line tidy.getParseErrors() == 0 will be true if getParseErrors() returns 0, meaning that no errors
+     * where found when parsing the HTML string. If getParseErrors() returns any number higher than 0 the validation
+     * has failed and there is at least one error in the HTML. Warnings are allowed and will not give any errors.
      */
     @Test
     public void testWriteDoc(){
@@ -35,26 +30,51 @@ public class DocumentBuilderTest {
         commits.add(new CommitStructure("79979779977979", "11/02-20",
                 "logloglogsfsssf645646465466sfsffs", true));
 
-        DocumentBuilder db = new DocumentBuilder();
+        //set system out to an outputstream so it does not print anything when
+        //running tidy.parse(System.in, System.out)
+        System.setOut(new PrintStream(new OutputStream() {
+            public void write(int b) {}}));
+
+        String html = db.writeDoc(commits);
+        assertTrue(html.contains("23424324432424"));
+        assertTrue(html.contains("79979779977979"));
+
+        //https://stackoverflow.com/questions/26608365/how-to-validate-html-using-jtidy
+        //http://jtidy.sourceforge.net/apidocs/index.html
+        Tidy tidy = new Tidy();
+        InputStream stream = new ByteArrayInputStream(html.getBytes());
+        tidy.parse(stream, System.out); //parse the html string
+        assertTrue(tidy.getParseErrors() == 0); //the number of errors that occurred in the most recent parse operation.
+        assertTrue(tidy.getParseWarnings() == 0); //the number of warnings that occurred in the most recent parse operation.
+    }
+
+    /**
+     * Test for the DocumentBuilder class.
+     * The first part of the test asserts that the returned HTML string from the method db.writeBuildDetails contains
+     * the build details from the commit passed to the method.
+     *
+     * The second part of the test uses Jtidy to parse the string containing the HTML and check for errors.
+     * The line tidy.getParseErrors() == 0 will be true if getParseErrors() returns 0, meaning that no errors
+     * where found when parsing the HTML string. If getParseErrors() returns any number higher than 0 the validation
+     * has failed and there is at least one error in the HTML. Warnings are allowed and will not give any errors.
+     */
+    @Test
+    public void testWriteBuilDetails(){
+
+        CommitStructure commit = new CommitStructure("23424324432424", "11/02-20",
+                "logloglogsfsssfsfsfsfsffs", true);
 
 
-        try {
-            String html = db.writeDoc(commits);
-            assertTrue(html.contains("23424324432424"));
-            assertTrue(html.contains("79979779977979"));
+        String html = db.writeBuildDetails(commit);
+        assertTrue(html.contains("logloglogsfsssfsfsfsfsffs"));
 
-            //https://stackoverflow.com/questions/23737300/how-to-validate-html-using-java-getting-issues-with-jsoup-library
+        //https://stackoverflow.com/questions/26608365/how-to-validate-html-using-jtidy
+        //http://jtidy.sourceforge.net/apidocs/index.html
+        Tidy tidy = new Tidy();
+        InputStream stream = new ByteArrayInputStream(html.getBytes());
+        tidy.parse(stream, System.out); //parse the html string
+        assertTrue(tidy.getParseErrors() == 0); //the number of errors that occurred in the most recent parse operation.
+        assertTrue(tidy.getParseWarnings() == 0); //the number of warnings that occurred in the most recent parse operation.
 
-            Document initialHtmlDoc = Jsoup.parse(html);
-            Document validatedHtmlDoc = Jsoup.connect("https://validator.w3.org/nu/")
-                    .header("content-type", "text/html; charset=UTF-8")
-                    .requestBody(initialHtmlDoc.html())
-                    .post();
-
-            assertFalse(validatedHtmlDoc.toString().toLowerCase().contains("there were errors"));
-        } catch(Exception e){
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
     }
 }
